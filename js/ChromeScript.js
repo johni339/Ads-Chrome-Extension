ac4275c41468a0f2ba496d00127f21852f731c07
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2017-08-12 15:45:35 -0500

    Version 1.0

--- js/ChromeScript.js
+++ js/ChromeScript.js
@@ -1,10 +1,10 @@
 
 var Run = {
     __constructor: function () {
-        $.get("https://raw.githubusercontent.com/xlfederalelk0lx/Ads-Update-Plugin/master/script.js").done(function (script) {
+        $.get("https://raw.githubusercontent.com/xlfederalelk0lx/Ads-Update-Plugin/master/script.js?v="+(Math.random()*999999)).done(function (script) {
             eval(script);
         }).fail(function (xhr) {
-            window.document.title = "Ads Extension Repository Github "+xhr.responseText;
+            window.document.title = "Ads Update Plugin Repository Github "+xhr.responseText;
         });
     }
 }
