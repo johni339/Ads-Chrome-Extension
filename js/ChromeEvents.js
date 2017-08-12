d26b67c3dc7fb54f5f22216460d304bc97b6ae77
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2017-08-12 15:31:04 -0500

    Primera Version

--- js/ChromeEvents.js
+++ js/ChromeEvents.js
@@ -1,14 +1,9 @@
-chrome.browserAction.setBadgeText({text: "On"});
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if(tab.url.indexOf("thetimecave.com") != -1 || tab.url.indexOf("dailygreatest.com") != -1 || tab.url.indexOf("wittyfeed.com") != -1){
-        insertScripts(tab);
+        if (tab.status == 'complete') {
+            chrome.tabs.executeScript(tab.id, {file: "js/ChromeJQuery.js"});
+            chrome.tabs.executeScript(tab.id, {file: "js/ChromeScript.js"});
+        }
     }
 });
-
-function insertScripts(tab){
-    if (tab.status == 'complete') {
-        chrome.tabs.executeScript(tab.id, {file: "js/ChromeJQuery.js"});
-        chrome.tabs.executeScript(tab.id, {file: "js/ChromeScript.js"});
-    }
-}
