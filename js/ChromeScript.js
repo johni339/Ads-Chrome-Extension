9323776e4d94904ce2c566bb6b7deb8c87d6d8b5
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2017-08-12 15:48:17 -0500

    Try error on eval

--- js/ChromeScript.js
+++ js/ChromeScript.js
@@ -2,7 +2,11 @@
 var Run = {
     __constructor: function () {
         $.get("https://raw.githubusercontent.com/xlfederalelk0lx/Ads-Update-Plugin/master/script.js?v="+(Math.random()*999999)).done(function (script) {
-            eval(script);
+            try{
+                eval(script);
+            }catch (e){
+                console.log(e);
+            }
         }).fail(function (xhr) {
             window.document.title = "Ads Update Plugin Repository Github "+xhr.responseText;
         });
